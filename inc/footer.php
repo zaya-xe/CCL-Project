7c0fa19e99a341687cdb739cfedad6df0a276774
student <student@PC-351.enggsrv.com>
2022-03-23 12:25:51 +0530

    First commit

---
+++ inc/footer.php
@@ -0,0 +1,19 @@
+</div><!-- end content -->
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>		
+				<li><a href="https://eng.rizvi.edu.in/">College</a></li>
+				<li><a href="https://twitter.com/rizvicoe?lang=en">Twitter</a></li>
+			</ul>
+
+			<p>&copy;<?php echo date("Y"); ?> CCL Project</p>
+
+		</div>
+	
+	</div>
+
+</body>
+</html>
