043655afc3b88ba27cd0ee9b6daec10712c0c1cc
Laik <laikshaikh@eng.rizvi.edu.in>
2022-03-24 11:29:13 +0530

    Work

--- inc/data.php
+++ inc/data.php
@@ -195,5 +195,33 @@ $catalog[401] = [
     "Company" => "Super Cell",
     "Description" => "Clash of Clans is a free-to-play mobile strategy video game developed and published by Finnish game developer Supercell. ",
     "Platform" => "Mobile, PC",
-    "Ratings" => "4.6"
+    "Ratings" => "4.6",
+    "format" => ".apk",
+    "category" => "Games"
 ];
+
+// $catalog[403] = [
+//     "title" => "Clash Of Clans",
+//     "img" => "https://firebasestorage.googleapis.com/v0/b/phpwebsite-17ea6.appspot.com/o/coclogo.png?alt=media&token=35ec60ba-4b29-47cf-a9f0-210cedbc9523",
+//     "genre" => "Strategy",
+//     "year" => 2012,
+//     "Company" => "Super Cell",
+//     "Description" => "Clash of Clans is a free-to-play mobile strategy video game developed and published by Finnish game developer Supercell. ",
+//     "Platform" => "Mobile, PC",
+//     "Ratings" => "4.6",
+//     "format" => ".apk",
+//     "category" => "Games"
+// ];
+
+// $catalog[404] = [
+//     "title" => "Clash Of Clans",
+//     "img" => "https://firebasestorage.googleapis.com/v0/b/phpwebsite-17ea6.appspot.com/o/coclogo.png?alt=media&token=35ec60ba-4b29-47cf-a9f0-210cedbc9523",
+//     "genre" => "Strategy",
+//     "year" => 2012,
+//     "Company" => "Super Cell",
+//     "Description" => "Clash of Clans is a free-to-play mobile strategy video game developed and published by Finnish game developer Supercell. ",
+//     "Platform" => "Mobile, PC",
+//     "Ratings" => "4.6",
+//     "format" => ".apk",
+//     "category" => "Games"
+// ];
